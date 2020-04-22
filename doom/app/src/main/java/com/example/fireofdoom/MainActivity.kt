8ed63e49d45c2bc6ea7992cbb93c126be9095b2e
MehaGarg <meha.garg@gotinder.com>
2020-04-21 18:02:13 -0700

    Rect on loop

---
+++ doom/app/src/main/java/com/example/fireofdoom/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.fireofdoom
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.compose.Model
+import androidx.compose.Recompose
+import androidx.compose.state
+import androidx.ui.core.DrawScope
+import androidx.ui.core.Modifier
+import androidx.ui.core.setContent
+import androidx.ui.foundation.Canvas
+import androidx.ui.geometry.Rect
+import androidx.ui.graphics.Color
+import androidx.ui.graphics.Paint
+import androidx.ui.graphics.PaintingStyle
+import androidx.ui.graphics.withSave
+import androidx.ui.layout.fillMaxSize
+import androidx.ui.material.MaterialTheme
+import androidx.ui.tooling.preview.Preview
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val state = DoomState()
+            DoomCompose(state)
+            val handler = Handler()
+
+            val propagate: Runnable = object : Runnable {
+                override fun run() {
+                    handler.postDelayed(this, 500)
+                    state.offset = 300f + (Random.nextFloat() * 100)
+                }
+            }
+            propagate.run()
+        }
+    }
+}
+
+@Model
+data class DoomState(var offset: Float = 300f + (Random.nextFloat()*100))
+
+
+@Composable
+fun DoomCompose(
+    state: DoomState
+) {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        withSave { drawRect(state.offset) }
+    }
+}
+
+private fun DrawScope.drawRect(offset: Float) {
+    drawRect(
+        rect = Rect(
+            left = 0f + offset,
+            top = 0f + offset,
+            right = size.width.value - offset,
+            bottom = size.height.value - offset
+        ),
+        paint = Paint().apply {
+            style = PaintingStyle.fill
+            color = Color.Red
+        }
+    )
+}
